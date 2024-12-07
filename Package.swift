9b4b590282dcf71c4736f2ad241cbd338363a1b0
Nathan Lanza <nathanlanza@gmail.com>
2024-12-07 18:32:14 -0500

    Port build to be based on a Swift package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
-  name: "SwiftOrg"
+  name: "SwiftOrg",
+  products: [
+    .library(name: "SwiftOrg", targets: ["SwiftOrg"]),
+  ],
+  targets: [
+    .target(name: "SwiftOrg"),
+    .testTarget(name: "SwiftOrgTests", dependencies: ["SwiftOrg"]),
+  ]
 )
