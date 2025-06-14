6c476ce12951fb7a5633cefadd113cb1a2781482
Nathan Lanza <nathanlanza@gmail.com>
2025-06-13 22:02:56 -0400

    Implement OrgFileWriter test

--- Tests/SwiftOrgTests/OrgFileWriterTests.swift
+++ Tests/SwiftOrgTests/OrgFileWriterTests.swift
@@ -11,15 +11,45 @@ import XCTest
 
 class OrgFileWriterTests: XCTestCase {
 
-  // func testOrgFileWriter() throws {
+  func testOrgFileWriter() throws {
+    // Use the README.org content directly instead of trying to load from bundle
+    let content = """
+      #+TITLE: SwiftOrg
 
-  //     let path = Bundle(for: type(of: self)).path(forResource: "README", ofType: "org")
-  //     let content = try String(contentsOfFile: path!)
-  //     let parser = OrgParser()
-  //     let doc = try parser.parse(content: content)
+      * org-mode Parser for Swift
 
-  //     _ = doc.toText()
-  //     // print(text)
-  // }
+        [[https://travis-ci.org/xiaoxinghu/swift-org.svg?branch=master]]
+        [[https://img.shields.io/github/license/mashape/apistatus.svg?maxAge=2592000]]
+        [[https://img.shields.io/badge/Carthage-compatible-4BC51D.svg?style=flat]]
+        [[https://img.shields.io/github/release/xiaoxinghu/swift-org.svg?maxAge=2592000]]
+
+        [[http://orgmode.org/][org-mode]] is awesome. This is the first step to bring it to iOS, (arguably) the
+        most popular platform on the planet.
+
+      * Usage
+        An simple example will explain everything.
+
+        #+BEGIN_SRC swift
+          import SwiftOrg
+
+          let lines = [
+              "* TODO head line",
+              "  A normal line here.",
+          ]
+          let parser = OrgParser()
+          let doc = try parser.parse(lines: lines)
+        #+END_SRC
+      """
+
+    let parser = OrgParser()
+    let doc = try parser.parse(content: content)
+
+    let text = doc.toText()
+    XCTAssertFalse(text.isEmpty)
+    // Verify basic structure is preserved
+    XCTAssertTrue(text.contains("#+TITLE: SwiftOrg"))
+    XCTAssertTrue(text.contains("* org-mode Parser for Swift"))
+    XCTAssertTrue(text.contains("* Usage"))
+  }
 
 }
