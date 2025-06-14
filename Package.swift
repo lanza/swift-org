a400571684ea7bf4f68df5dc58359f9817e6ad84
Nathan Lanza <nathanlanza@gmail.com>
2025-06-13 21:57:14 -0400

    Cean up package file

--- Package.swift
+++ Package.swift
@@ -1,13 +1,24 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 import PackageDescription
 
 let package = Package(
   name: "SwiftOrg",
+  platforms: [
+    .macOS(.v10_15),
+    .iOS(.v13),
+    .tvOS(.v13),
+    .watchOS(.v6)
+  ],
   products: [
     .library(name: "SwiftOrg", targets: ["SwiftOrg"])
   ],
   targets: [
-    .target(name: "SwiftOrg"),
-    .testTarget(name: "SwiftOrgTests", dependencies: ["SwiftOrg"]),
+    .target(
+      name: "SwiftOrg"
+    ),
+    .testTarget(
+      name: "SwiftOrgTests",
+      dependencies: ["SwiftOrg"]
+    ),
   ]
 )
